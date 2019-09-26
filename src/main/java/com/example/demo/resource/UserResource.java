79038f7a2a0b1cee76439fdaec1d29da68f72f36
Pramuditha Kaushalya <pramuditha.kaushalya@gmail.com>
2019-09-26 13:11:11 +0530

    Change Password

--- src/main/java/com/example/demo/resource/UserResource.java
+++ src/main/java/com/example/demo/resource/UserResource.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.model.User;
+import com.example.demo.model.UserFavorite;
+import com.example.demo.repository.UserFavoriteRepository;
 import com.example.demo.repository.UserRepository;
 
 @CrossOrigin(origins = "*")
@@ -14,6 +16,9 @@ public class UserResource {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private UserFavoriteRepository userFavoriteRepository;
 
     @GetMapping("/all")
     public List<User> getAll() {
@@ -33,6 +38,8 @@ public class UserResource {
         String pwd = userData.password;
         Integer id = userData.getUser_id();
         System.out.println("User ID"+id);
+        System.out.println("User Password"+password);
+        System.out.println("User db passsword"+pwd);
         if(password.equals(pwd)){
             return id;
         } else{
@@ -40,11 +47,20 @@ public class UserResource {
         }
     }
 
+    @PostMapping(value = "/login")
+    public User login(@RequestBody User user ) {
+        String name= user.name;
+        return userRepository.findByName(name);
+    }
+
     @GetMapping("/delete/{id}")
-    public String DeleteById(@PathVariable("id") Integer id) {
-        userRepository.deleteById(id);
+    public String DeleteById(@PathVariable("id") User id) {
+        Integer uid = id.getUser_id();
+        UserFavorite userFavorite = userFavoriteRepository.findByUser(id);
+        Integer userfavid = userFavorite.getId();
+        userFavoriteRepository.deleteById(userfavid);
+        userRepository.deleteById(uid);
         return "User Delete Successfully!!!";
-        
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/update/{id}")
@@ -85,5 +101,24 @@ public class UserResource {
         } else{
             return false;
         }
+    }
+
+    @GetMapping("/change/{id}")
+    public Optional<User> changePassword(@PathVariable("id") Integer id) {
+        return userRepository.findById(id);
+        
+    }
+
+    @PostMapping(value = "/changepassword")
+    public Boolean changeNew(@RequestBody User user) {
+        Integer id = user.getUser_id();
+        String password = user.password;
+
+        User data = userRepository.getOne(id);
+
+        data.setPassword(password);
+        userRepository.save(data);
+        return true;
+        
     }
 }
